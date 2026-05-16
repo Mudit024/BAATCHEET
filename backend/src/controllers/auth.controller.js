b1c4518edfa2d7ab59f476a5f5ad9c6d8b969274
Mudit024 <jawaramudit123@gmail.com>
2026-05-16 14:39:54 +0530

    login and logout created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import bcrypt from "bcryptjs";
 import { sendEmail } from "../lib/mail.js";
 import { welcomeEmailTemplate } from "../templates/welcomeEmailTemplate.js";
 
-// this is the controller of sign up 
+// 1.....this is the controller of sign up 
 export const signup = async (req, res) => {
      const { fullName, email, password } = req.body;
 
@@ -42,7 +42,7 @@ export const signup = async (req, res) => {
                          "Welcome to BAATCHEET 🚀",
                          welcomeEmailTemplate(savedUser.fullName)
                     );
-                        
+
                     res.status(201).json({
                          _id: newUser._id,
                          fullName: newUser.fullName,
@@ -59,3 +59,32 @@ export const signup = async (req, res) => {
           res.status(400).json({ message: "Internal error" });
      }
 };
+
+// 2.....this is controller for login 
+
+export const login = async (req, res) => {
+     const { email, password } = req.body
+     try {
+          const user = await User.findOne({ email })
+          if (!user) return res.status(400).json({ message: "Invalid Credentials" })
+
+          const isPasswordCorrect = await bcrypt.compare(password, user.password)
+          if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid Credentials" })
+
+          generateToken(user._id, res)
+          res.status(200).json({
+               _id: user._id,
+               fullName: user.fullName,
+               email: user.email,
+               profilePic: user.profilePic,
+          });
+     } catch (error) {
+          console.error("Error in the login controller", error);
+          res.status(500).json({ message: "Internal Server error" });
+     }
+};
+
+export const logout = (__,res) => {
+     res.cookie("jwt", "", { maxAge: 0 })
+     res.status(200).json({ message: "Logged Out successfully" });
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,13 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signup,login,logout } from "../controllers/auth.controller.js";
 
 const router=express.Router();
 
+// LOGIN LOGOUT AND SIGNUP 
 router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout)
 
-router.get("login",(req,res) => {
-      res.send("login endpoint");
-})
 
-router.get("logout",(req,res) => {
-      res.send("logoutendpoint");
-})
 
 export default router;
